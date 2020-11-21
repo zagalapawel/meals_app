46cfb9fe4405bd9ebe102c5048bc1fa276929aef
zagalapawel <zagalapawel@gmail.com>
2020-11-21 19:19:18 +0100

    + meals images

--- lib/widgets/meal_item.dart
+++ lib/widgets/meal_item.dart
@@ -1,6 +1,21 @@
 import 'package:flutter/material.dart';
+import '../models/meal.dart';
 
 class MealItem extends StatelessWidget {
+  final String title;
+  final String imageUrl;
+  final int duration;
+  final Complexity complexity;
+  final Affordability affordability;
+
+  MealItem({
+    @required this.title,
+    @required this.imageUrl,
+    @required this.duration,
+    @required this.complexity,
+    @required this.affordability,
+  });
+
   void selectMeal() {}
 
   @override
@@ -15,7 +30,20 @@ class MealItem extends StatelessWidget {
         margin: EdgeInsets.all(10),
         child: Column(children: <Widget>[
           Stack(
-            children: <Widget>[],
+            children: <Widget>[
+              ClipRRect(
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(15),
+                  topRight: Radius.circular(15),
+                ),
+                child: Image.network(
+                  imageUrl,
+                  height: 250,
+                  width: double.infinity,
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ],
           ),
         ]),
       ),
