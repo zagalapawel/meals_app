807f19121816c63f1acdc7f088dcc635cc152f50
zagalapawel <zagalapawel@gmail.com>
2020-11-22 19:24:20 +0100

    +temp deleting recipes

--- lib/screens/meal_detail_screen.dart
+++ lib/screens/meal_detail_screen.dart
@@ -89,6 +89,13 @@ class MealDetailScreen extends StatelessWidget {
           ],
         ),
       ),
+      floatingActionButton: FloatingActionButton(
+          child: Icon(
+            Icons.delete,
+          ),
+          onPressed: () {
+            Navigator.of(context).pop(mealId);
+          }),
     );
   }
 }
